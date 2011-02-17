390a15e233728c9e14f22ef12d443a52eac55f40
Stuart Campbell <stuart@harto.org>
2011-02-17 13:41:17 +1100

    implement better collision processing

--- breakout.js
+++ breakout.js
@@ -46,6 +46,8 @@ var UPDATE_HZ = 20,
 
     BRICK_W = (SCREEN_W - 2 * WALL_W) / BRICK_COLS,
     BRICK_H = 15,
+    // FIXME: consolidate
+    BRICK_Y_OFFSET = WALL_H + 3 * BRICK_H,
     BOTTOM_ROW_Y = WALL_H + (BRICK_ROWS + 3) * BRICK_H,
 
     PADDLE_W = 2 * BRICK_W,
@@ -57,6 +59,19 @@ var UPDATE_HZ = 20,
 
     REINSERT_DELAY = 2000,
     LIVES = 3,
+
+    // game states
+    State = {
+        RUNNING: 1,
+        REINSERT: 2,
+        FINISHED: 3
+    },
+
+    // directions
+    NORTH = 1,
+    SOUTH = 2,
+    EAST = 4,
+    WEST = 16,
 
     walls,
     paddle,
@@ -71,14 +86,6 @@ var UPDATE_HZ = 20,
     state,
     dropoutTime;
 
-/// game state
-
-var State = {
-    RUNNING: 1,
-    REINSERT: 2,
-    FINISHED: 3
-};
-
 /// ball
 
 function Ball() {
@@ -87,8 +94,8 @@ function Ball() {
 
     this.radius = BALL_SIZE / 2;
 
-    this.vx = BALL_SPEED;
-    this.vy = -BALL_SPEED;
+    this.speed = BALL_SPEED;
+    this.angle(NORTH | EAST);
 }
 
 Ball.prototype = {
@@ -112,6 +119,7 @@ Ball.prototype = {
         this.y += this.vy;
     },
 
+    // compute angle of deflection from collision with object
     collision: function (o) {
         var x = this.x, x2 = this.x + BALL_SIZE,
             y = this.y, y2 = this.y + BALL_SIZE,
@@ -121,28 +129,24 @@ Ball.prototype = {
         var collides = ((ox <= x && x <= ox2) || (ox <= x2 && x2 <= ox2)) &&
                        ((oy <= y && y <= oy2) || (oy <= y2 && y2 <= oy2));
 
-        if (collides && x < ox) {
-            this.vx = -Math.abs(this.vx);
-        } else if (collides && ox2 < x2) {
-            this.vx = Math.abs(this.vx);
-        }
-
-        if (collides && y < oy) {
-            this.vy = -Math.abs(this.vy);
-        } else if (collides && oy2 < y2) {
-            this.vy = Math.abs(this.vy);
-        }
+        this.angle((!collides ? 0 : x < ox ? WEST : ox2 < x2 ? EAST : 0) |
+                   (!collides ? 0 : y < oy ? NORTH : oy2 < y2 ? SOUTH : 0));
 
         return collides;
     },
 
-    outOfBounds: function () {
-        return SCREEN_H <= this.y;
+    angle: function (d) {
+        this.vx = d & WEST ? -this.speed :
+                  d & EAST ? this.speed :
+                  this.vx;
+
+        this.vy = d & NORTH ? -this.speed :
+                  d & SOUTH ? this.speed :
+                  this.vy;
     },
 
-    toString: function () {
-        return 'Ball[x=' + this.x + ', y=' + this.y +
-               ', size=' + BALL_SIZE + ']';
+    outOfBounds: function () {
+        return SCREEN_H <= this.y;
     }
 };
 
@@ -177,11 +181,11 @@ Paddle.prototype.move = function (direction) {
 /// bricks
 
 function Brick(col, row) {
-    this.colour = ROW_COLOURS[BRICK_ROWS - 1 - row];
+    this.colour = ROW_COLOURS[row];
     this.value = 2 * Math.floor(row / 2) + 1;
 
     this.x = WALL_W + col * BRICK_W;
-    this.y = BOTTOM_ROW_Y - row * BRICK_H;
+    this.y = BRICK_Y_OFFSET + row * BRICK_H;
 
     this.w = BRICK_W;
     this.h = BRICK_H;
@@ -189,9 +193,16 @@ function Brick(col, row) {
 
 Brick.prototype = new Rectangle();
 
-Brick.prototype.toString = function () {
-    return 'Brick[x=' + this.x + ', y=' + this.y +
-           ', colour=' + this.colour + ', value=' + this.value + ']';
+Brick.init = function () {
+    var bricks = [];
+    for (var y = 0; y < BRICK_ROWS; y++) {
+        var row = [];
+        for (var x = 0; x < BRICK_COLS; x++) {
+            row.push(new Brick(x, y));
+        }
+        bricks.push(row);
+    }
+    return bricks;
 };
 
 /// playing area
@@ -212,8 +223,13 @@ function draw(ctx) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, SCREEN_W, SCREEN_H);
 
-    walls.concat(bricks, ball, paddle).forEach(function (o) {
+    walls.concat(ball, paddle).forEach(function (o) {
         o.draw(ctx);
+    });
+    bricks.forEach(function (row) {
+        row.forEach(function (brick) {
+            brick.draw(ctx);
+        });
     });
 
     ctx.font = 'bold 10px Helvetica, Arial, sans-serif';
@@ -228,39 +244,70 @@ function draw(ctx) {
     if (paused) {
         ctx.fillText('<Paused>', SCREEN_W / 2, 2 * SCREEN_H / 3);
     } else if (state === State.FINISHED) {
-        ctx.fillText('Game over\n:( :( :( :( :( :( :(', SCREEN_W / 2, 2 * SCREEN_H / 3);
+        ctx.fillText('Press <N> to restart', SCREEN_W / 2, 2 * SCREEN_H / 3);
     }
 }
 
 var movingLeft = false,
     movingRight = false;
 
-function update() {
-    if (paused) {
-        return;
+function processCollisions() {
+    // simple collision detection for walls
+    if (ball.x <= WALL_W) {
+        ball.angle(EAST);
+    } else if (SCREEN_W - WALL_W - BALL_SIZE < ball.x) {
+        ball.angle(WEST);
+    }
+    if (ball.y <= WALL_H) {
+        ball.angle(SOUTH);
     }
 
-    // process collisions
+    ball.collision(paddle);
 
-    walls.concat(paddle).forEach(function (o) {
-        ball.collision(o);
+    // Determine which rows need per-brick inspection
+    var rows = bricks.filter(function (_, i) {
+        var by = ball.y,
+            by2 = ball.y + BALL_SIZE,
+            ry = BRICK_Y_OFFSET + i * BRICK_H,
+            ry2 = ry + BRICK_H;
+        return (ry <= by && by <= ry2) || (ry <= by2 && by2 <= ry2);
     });
 
-    for (var i = 0; i < bricks.length; i++) {
-        var brick = bricks[i];
-        if (ball.collision(brick)) {
-            score += brick.value;
-            bricks.splice(i, 1);
-            break;
+    // Traverse bricks according to the current direction of the ball. This
+    // resolves any ambiguity about the deflection angle or which brick to
+    // remove when the ball strikes two or more bricks at once.
+
+    var n2s = ball.vy > 0;
+    var w2e = ball.vx > 0;
+
+    var rowStart = n2s ? 0 : rows.length - 1;
+    var rowEnd = n2s ? rows.length : -1;
+    var rowInc = n2s ? 1 : -1;
+
+    var colInc = w2e ? 1 : -1;
+
+    outer:
+    for (var y = rowStart; y != rowEnd; y += rowInc) {
+        var row = rows[y];
+        var colStart = w2e ? 0 : row.length - 1;
+        var colEnd = w2e ? row.length : -1;
+        for (var x = colStart; x != colEnd; x += colInc) {
+            var brick = row[x];
+            if (ball.collision(brick)) {
+                row.splice(x, 1);
+                score += brick.value;
+                break outer;
+            }
         }
     }
+}
 
-    // update state
-
+function update() {
     paddle.move(movingLeft ? -1 : movingRight ? 1 : 0);
 
     switch (state) {
     case State.RUNNING:
+        processCollisions();
         ball.update();
         if (ball.outOfBounds()) {
             state = (--lives === 0) ? State.FINISHED : State.REINSERT;
@@ -279,7 +326,9 @@ function update() {
 var timer, nextLoopTime, ctx;
 
 function loop() {
-    update();
+    if (!paused) {
+        update();
+    }
     draw(ctx);
 
     if (state !== State.FINISHED) {
@@ -302,13 +351,7 @@ function newGame() {
 
     ball = new Ball();
     paddle = new Paddle();
-
-    bricks = [];
-    for (var row = 0; row < BRICK_ROWS; row++) {
-        for (var col = 0; col < BRICK_COLS; col++) {
-            bricks.push(new Brick(col, row));
-        }
-    }
+    bricks = Brick.init();
 
     nextLoopTime = +new Date();
     timer = window.setTimeout(loop, UPDATE_DELAY);
